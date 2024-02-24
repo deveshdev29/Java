5f786988febaddb17e64f168f6e363355c637e21
Devesh <137254752+deveshdev29@users.noreply.github.com>
2024-02-24 20:00:29 +0530

    Add files via upload

---
+++ age.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+
+public class age {
+    public static void main(String[] arg){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("What's Your Age: ");
+        int age = sc.nextInt();
+
+        if(age>18){
+            System.out.println("You are an adult");
+        }
+        else{
+            System.out.println("You are not an adult");
+        }
+        sc.close();
+    }
+    
+}

c940f8a5c70a1aaecbb79dd8c5b9a8edce5e4186
Devesh <137254752+deveshdev29@users.noreply.github.com>
2024-03-02 10:19:07 +0530

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class loops {
+    public static void main(String[] arg){
+
+        Scanner sc = new Scanner(System.in);
+
+        for(int count=0;count<100;count++){
+            System.out.println("HELLO");
+        }
+
+        sc.close();
+    }
+    
+}

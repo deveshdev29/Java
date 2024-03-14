222f87cd6a89d245178e58a7715ca1d624d61d0a
Devesh <137254752+deveshdev29@users.noreply.github.com>
2024-03-14 08:46:32 +0530

    Add files via upload

---
+++ exam.java
@@ -0,0 +1,8 @@
+public class exam{
+    public static void main(String args[]){
+        String arr[] = {"Bennett","University"};
+
+        System.out.println(arr[0].charAt(0));
+        System.out.println(arr[0].indexOf("n"));
+    }
+}
